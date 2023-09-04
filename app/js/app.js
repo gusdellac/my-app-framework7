9c8f3ae33d66471d9692cdf8a02af3c90420ca35
Gustavo DeLlac <gustavodellac@gmail.com>
2023-09-04 12:38:52 -0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+var app = new Framework7({
+    root: "#app",
+    name: "My App",
+    id:  "com.myapp",
+    panel: {
+        swipe: "left",
+    },
+    routes: [
+        {
+            path: "/home/",
+            url: "index.html",
+        },
+    ],
+});
+
+var mainView = app.views.create(".view-main");
