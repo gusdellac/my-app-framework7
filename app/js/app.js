46a09e6d2dcab8e8d765a76e8fd5897790026314
Gustavo DeLlac <gustavodellac@gmail.com>
2023-09-06 11:57:19 -0300

    pagina galeria, panel left con links, popup autoejecutable

--- app/js/app.js
+++ app/js/app.js
@@ -13,8 +13,26 @@ var app = new Framework7({
         {
             path: "/usuario/",
             url: "usuarios.html",
+        },
+        {
+            path: "/galeria/",
+            url: "galeria.html",
         }
     ],
 });
 
 var mainView = app.views.create(".view-main");
+
+//abrir el popup
+var abrirPopupInicial = app.popup.open(".popup-inicial");
+
+//cerrar el popup
+var cerrarPopup;
+
+function cerrarPopup(){
+    cerrarPopup = setTimeout(() => {
+        abrirPopupInicial = app.popup.close(".popup-inicial", true);
+    }, 3000);
+};
+
+cerrarPopup();
